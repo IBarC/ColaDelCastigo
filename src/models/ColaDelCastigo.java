1a7746a69df9e07d06bbad5ca0f92fd2564d8543
Irene <a_irene.barboteo.company@iespablopicasso.es>
2022-02-21 13:03:03 +0100

    Creación de la clase

---
+++ src/models/ColaDelCastigo.java
@@ -0,0 +1,79 @@
+package models;
+
+import java.util.Collection;
+import java.util.Iterator;
+
+public class ColaDelCastigo<T> {
+
+	public int size() {
+		return 0;
+	}
+
+	public boolean isEmpty() {
+		return false;
+	}
+
+	public boolean contains(Object o) {
+		return false;
+	}
+
+	public Iterator iterator() {
+		return null;
+	}
+
+	public Object[] toArray() {
+		return null;
+	}
+
+	public Object[] toArray(Object[] a) {
+		return null;
+	}
+
+	public boolean remove(Object o) {
+		return false;
+	}
+
+	public boolean containsAll(Collection c) {
+		return false;
+	}
+
+	public boolean addAll(Collection c) {
+		return false;
+	}
+
+	public boolean removeAll(Collection c) {
+		return false;
+	}
+
+	public boolean retainAll(Collection c) {
+		return false;
+	}
+
+	public void clear() {
+	}
+
+	public boolean add(Object e) {
+		return false;
+	}
+
+	public boolean offer(Object e) {
+		return false;
+	}
+
+	public Object remove() {
+		return null;
+	}
+
+	public Object poll() {
+		return null;
+	}
+
+	public Object element() {
+		return null;
+	}
+
+	public Object peek() {
+		return null;
+	}
+
+}
